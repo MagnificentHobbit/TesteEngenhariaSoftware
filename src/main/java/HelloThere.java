9cc749113df32aa5422d301e8f5cdbbf7f508e79
Rui Araujo <8230112@estg.ipp.pt>
2024-10-06 18:57:04 +0100

    Apenas com a função greeting.getGreeting()

---
+++ src/main/java/HelloThere.java
@@ -0,0 +1,8 @@
+public class HelloThere {
+    public HelloThere() {
+    }
+
+    public void getGreeting() {
+        System.out.println("Hello there");
+    }
+}
---
+++ src/main/java/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        HelloThere greeting = new HelloThere();
+
+        greeting.getGreeting();
+    }
+}
